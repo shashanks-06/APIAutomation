af8708d533fb37e8b3b194aa3d1b9a4f6b10293d
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-09-18 12:15:42 +0530

    Builder Design Pattern in  Java

---
+++ src/test/java/Shashank/ApiAutomation/September2024/exe_15092024/BuilderDesignPatternJava.java
@@ -0,0 +1,24 @@
+package Shashank.ApiAutomation.September2024.exe_15092024;
+
+public class BuilderDesignPatternJava {
+    // Change return type of each method as a Class type
+    // "this" always points to the current/calling object. Returning the same to have same reference
+
+    public BuilderDesignPatternJava Floor1(){
+        System.out.println("Floor 1 is done");
+        return this;
+    }
+    public BuilderDesignPatternJava Floor2(String param){
+        System.out.println("Floor 2 is done");
+        return this;
+    }
+    public BuilderDesignPatternJava Floor3(){
+        System.out.println("Floor 3 is done");
+        return this;
+    }
+
+    public static void main(String[] args) {
+        BuilderDesignPatternJava bdpj = new BuilderDesignPatternJava();
+        bdpj.Floor1().Floor2("Shashank").Floor3();
+    }
+}
---
+++ src/test/java/Shashank/ApiAutomation/September2024/exe_15092024/NoDesignPattern.java
@@ -0,0 +1,20 @@
+package Shashank.ApiAutomation.September2024.exe_15092024;
+
+public class NoDesignPattern {
+    public void stage1(){
+        System.out.println("Stage 1");
+    }
+    public void stage2(String str){
+        System.out.println("Stage 2");
+    }
+    public void stage3(){
+        System.out.println("Stage 3");
+    }
+
+    public static void main(String[] args) {
+        NoDesignPattern ndp = new NoDesignPattern();
+        ndp.stage1();
+        ndp.stage2("Shashank");
+        ndp.stage3();
+    }
+}
