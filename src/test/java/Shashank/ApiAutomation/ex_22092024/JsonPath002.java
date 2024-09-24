0a1a9dce261331178c6288967fac3ee7df8969cf
shashanks-06 <surjekarshashank@gmail.com>
2024-09-24 17:39:58 +0530

    response with JsonPath

---
+++ src/test/java/Shashank/ApiAutomation/ex_22092024/JsonPath002.java
@@ -0,0 +1,47 @@
+package Shashank.ApiAutomation.ex_22092024;
+
+public class JsonPath002 {
+    public static void main(String[] args) {
+        String response = "{\n" +
+                "  \"squadName\": \"Super hero squad\",\n" +
+                "  \"homeTown\": \"Metro City\",\n" +
+                "  \"formed\": 2016,\n" +
+                "  \"secretBase\": \"Super tower\",\n" +
+                "  \"active\": true,\n" +
+                "  \"members\": [\n" +
+                "    {\n" +
+                "      \"name\": \"Molecule Man\",\n" +
+                "      \"age\": 29,\n" +
+                "      \"secretIdentity\": \"Dan Jukes\",\n" +
+                "      \"powers\": [\n" +
+                "        \"Radiation resistance\",\n" +
+                "        \"Turning tiny\",\n" +
+                "        \"Radiation blast\"\n" +
+                "      ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"Madame Uppercut\",\n" +
+                "      \"age\": 39,\n" +
+                "      \"secretIdentity\": \"Jane Wilson\",\n" +
+                "      \"powers\": [\n" +
+                "        \"Million tonne punch\",\n" +
+                "        \"Damage resistance\",\n" +
+                "        \"Superhuman reflexes\"\n" +
+                "      ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"Eternal Flame\",\n" +
+                "      \"age\": 1000000,\n" +
+                "      \"secretIdentity\": \"Unknown\",\n" +
+                "      \"powers\": [\n" +
+                "        \"Immortality\",\n" +
+                "        \"Heat Immunity\",\n" +
+                "        \"Inferno\",\n" +
+                "        \"Teleportation\",\n" +
+                "        \"Interdimensional travel\"\n" +
+                "      ]\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+    }
+}
